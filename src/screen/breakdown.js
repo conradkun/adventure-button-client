8ccfe01cb16cec69f95b4fcfcf5cd25b15486295
silverbackking <me@justinglibert.com>
2017-06-28 18:18:51 +0200

    Experimenting with some UI for breakdown

--- src/screen/breakdown.js
+++ src/screen/breakdown.js
@@ -1,12 +1,22 @@
 import React, {Component} from 'react';
 import Box from 'grommet/components/Box';
+import Button from 'grommet/components/Button';
 import Header from 'grommet/components/Header';
 import Anchor from 'grommet/components/Anchor';
 import Title from 'grommet/components/Title';
 import AppSettings from '../utils/app_settings';
 import Spinner from 'react-spinkit';
 import MenuIcon from 'grommet/components/icons/base/Menu';
+import Add from 'grommet/components/icons/base/Add';
+import Close from 'grommet/components/icons/base/Close';
 import Search from 'grommet/components/Search';
+import Drag from 'grommet/components/icons/base/Drag';
+import Robot from 'grommet/components/icons/base/Robot';
+import FlipMove from 'react-flip-move';
+import Pulse from 'grommet/components/icons/Pulse';
+import Tip from 'grommet/components/Tip';
+
+
 import {
   SortableContainer,
   SortableElement,
@@ -15,45 +25,39 @@ import {
 } from 'react-sortable-hoc';
 
 
-const DragHandle = SortableHandle(() => <span>::</span>); // This can be any component you want
+const DragHandle = SortableHandle(() => <Anchor icon={<Drag />}/>); // This can be any component you want
 
-const SortableItem = SortableElement(({value}) => {
-  return (
-    <Box margin='medium' className="drop-shadow" colorIndex='light-1'>
-      <DragHandle />
-      {value}
-    </Box>
-  );
-});
-
-const SortableList = SortableContainer(({items}) => {
-  return (
-    <ul>
-      {items.map((value, index) => (
-        <SortableItem key={`item-${index}`} index={index} value={value} />
-      ))}
-    </ul>
-  );
-});
-
-class SortableComponent extends Component {
-  state = {
-    items: ['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6'],
-  };
-  onSortEnd = ({oldIndex, newIndex}) => {
-    let {items} = this.state;
-
-    this.setState({
-      items: arrayMove(items, oldIndex, newIndex),
-    });
-  };
-  render() {
-    let {items} = this.state;
-
-    return <SortableList lockAxis='y' items={items} onSortEnd={this.onSortEnd} useDragHandle={true} />;
+class SortableItemClass extends React.Component {
+  render () {
+    return(
+      <Box responsive={false} margin='medium' pad='small' direction='row' align='center' justify='between' className="drop-shadow" colorIndex='light-1'>
+        <DragHandle />
+        {this.props.value}
+        <Anchor icon={<Close />}
+          onClick={()=>{
+            this.props.onDeleteItem(this.props.value);
+          }}
+          />
+      </Box>
+    )
   }
 }
 
+
+const SortableItem = SortableElement(SortableItemClass);
+
+const SortableList = SortableContainer(({items, onDeleteItem}) => {
+  return (
+    <div>
+      <FlipMove staggerDelayBy={150} appearAnimation="accordionVertical" enterAnimation='fade' leaveAnimation={false} duration={750}
+        easing="ease-out">
+        {items.map((value, index) => (
+          <SortableItem key={`item-${index}`} index={index} onDeleteItem={onDeleteItem} value={value} />
+        ))}
+      </FlipMove>
+    </div>
+  );
+});
 
 class Breakdown extends Component {
 
@@ -64,6 +68,8 @@ class Breakdown extends Component {
 
         this.state = {
           isLoading: false,
+          items: ['Item 1', 'Item 2', 'Item 3'],
+          i: 4
         }
     }
 
@@ -71,14 +77,49 @@ class Breakdown extends Component {
       this._load();
     }
 
+    onSortEnd = ({oldIndex, newIndex}) => {
+      let {items} = this.state;
+
+      this.setState({
+        items: arrayMove(items, oldIndex, newIndex),
+      });
+    };
+
+    onDeleteItem = (id) => {
+      this.setState({
+        items: this.state.items.filter((i)=>{return i !== id}),
+
+      })
+    }
+
     _load() {
     }
 
     _renderContent() {
+      let {items} = this.state;
       return (
-          <Box basis='full' margin='large'>
-            <SortableComponent/>
-          </Box>
+
+            <Box size='full' margin='large' basis='full' direction='row' flex={true} justify='between'>
+              <Box direction='column' basis='1/2' className='no-margin'>
+                <SortableList lockAxis='y' items={items} onDeleteItem={this.onDeleteItem} onSortEnd={this.onSortEnd} lockToContainerEdges={true} useDragHandle={true} />
+                <Box basis='full' align='center'>
+                  <Button 
+                    icon={<Add style={{stroke :'#FFF'}}/>}
+                    className='better_button animate-all'
+                    onClick={()=>{
+                      let items = this.state.items;
+                      items.push('Item ' + this.state.i)
+                      this.setState({
+                        items: items,
+                        i: this.state.i + 1
+                      })
+                    }}
+                    primary={true}
+                    />
+                </Box>
+              </Box>
+              <Box basis='1/2' margin='medium' className='drop-shadow' justify='center' align='center' colorIndex={AppSettings.cardColor}>Hey</Box>
+            </Box>
       )
   }
 
@@ -93,7 +134,6 @@ class Breakdown extends Component {
         </Title>
       );
       let mobileButton;
-      let search;
       let colorIndex = 'light-1';
       let justify = 'between';
       if ('single' === this.props.responsive) {
@@ -104,17 +144,13 @@ class Breakdown extends Component {
           <Anchor icon={< MenuIcon />} onClick={this.props.onMenuOpen}></Anchor>
         );
       }
-      search = (<Search inline={true} fill={true} size='medium' placeHolder='Rechercher' defaultValue={this.state.searchString} dropAlign={{
-        "right": "right"
-      }} onDOMChange={(e) => {
-        this.setState({searchString: e.target.value})
-      }}/>);
       return (
         <Header size='small' className="drop-shadow-bottom" colorIndex={colorIndex} fixed={true}>
           {appLogo}
-          <Box flex={true} justify={justify} pad="small" direction='row' responsive={false}>
+          <Box flex={true} justify={justify} pad="small" margin={{right: 'medium'}} direction='row' responsive={false}>
             {title}
             {mobileButton}
+            {this.state.hasTemplate && <Pulse id='actions' icon={<Robot style={{stroke: '#000'}}/>} />}
           </Box>
         </Header>
       );
