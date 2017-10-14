a51a59d54ab2a444a65ed81482ccd8dc4f4b1a7c
conradkun <conrichu@gmail.com>
2017-10-14 13:50:37 +0200

    Messin with grommet
    and more shit

--- src/screens/create_game.js
+++ src/screens/create_game.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react'
 import Button from 'grommet/components/Button';
-
+import Box from 'grommet/components/Box';
+import Pulse from 'grommet/components/icons/Pulse';
+import DeployIcon from 'grommet/components/icons/base/Deploy';
 
 export default class createGame extends Component {
   render() {
     
     return (
-      <div>
+      <Box justify='start'
+      align='center'
+      wrap={false}
+      pad='medium'
+      full = {true}
+      margin='small'
+      colorIndex='accent-6'>
+      <Pulse size='large' icon={<DeployIcon size='small' />} />
         Hello {this.props.userId}
         <Button label='Label'
         onClick={()=>{
             this.props.sendAction('test', {test: 'test'})
         }}
         href='#' />
-      </div>
+      </Box>
     )
   }
 }
