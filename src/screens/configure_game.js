03a721bb908f30e30712be7dcf1c1a212e356b12
conradkun <conrichu@gmail.com>
2017-10-14 15:43:46 +0200

    config_game update
    Added start date, end date and budget

--- src/screens/configure_game.js
+++ src/screens/configure_game.js
@@ -4,19 +4,74 @@ import Box from 'grommet/components/Box';
 import Pulse from 'grommet/components/icons/Pulse';
 import DeployIcon from 'grommet/components/icons/base/Cube';
 import {withRouter} from 'react-router';
+import Form from 'grommet/components/Form';
+import FormField from 'grommet/components/FormField';
+import DateTime from 'grommet/components/DateTime';
+import NumberInput from 'grommet/components/NumberInput';
+
 export default class comfigureGame extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      fromDate: undefined,
+      untilDate: undefined,
+      budget: 0
+    };
+    this.handleUntilChange = this.handleUntilChange.bind(this);
+    this.handleFromChange = this.handleFromChange.bind(this);
+    this.handleBudgetChange = this.handleBudgetChange.bind(this);
+  }
+  
+  handleBudgetChange(event){
+    this.setState({budget: event.value});
+  }
+
+  handleFromChange(event){
+    if (this.state.untilDate === undefined || event < this.state.untilDate) this.setState({fromDate: event});
+  }
+
+  handleUntilChange(event){
+    if (this.state.fromDate === undefined || this.state.fromDate < event) this.setState({untilDate: event});
+  }
   render() {
-    
+
     return (
       <Box justify='center'
-      align='center'
-      wrap={false}
-      pad='medium'
-      full = {true}
-      margin='small'
-      colorIndex='accent-6'>
-      {console.log('test')}
-      <Button/>
+        align='center'
+        wrap={false}
+        margin='medium'
+        full = {true}
+        colorIndex='accent-1'>
+      
+        <Form>
+          <h1>Travel from...</h1>
+          <FormField>
+            <DateTime id='id'
+              name='from'
+              format='DD/MM/YYYY'
+              value={this.state.fromDate}
+              onChange={this.handleFromChange} />
+          </FormField>
+        </Form>
+        <br/>
+        <Form>
+          <h1>...Until...</h1>
+          <FormField>
+            <DateTime id='id'
+              name='until'
+              format='DD/MM/YYYY'
+              value={this.state.untilDate}
+              onChange={this.handleUntilChange} />
+          </FormField>
+        </Form>
+        <br/>
+        <Form>
+          <h1>How much are you willing to spend?</h1>
+          <NumberInput value={this.state.budget}
+            name='budget'
+            onChange={this.handleBudgetChange} />
+        </Form>
+
       </Box>
     )
   }
